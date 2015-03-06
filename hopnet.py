d3ec43b2bca17b7a1d69ad28110deb8bf2012b8a
Jared Smith <smith@utk.edu>
2015-03-06 16:04:47 -0500

    added graphing abilities for first part and cleaned up code

--- hopnet.py
+++ hopnet.py
@@ -6,19 +6,14 @@
 '''
 
 # Built-in Python libraries
-import sys
 import os
 import random
-import datetime
-import math
 import contextlib
 import argparse
-import tempfile
 from argparse import RawTextHelpFormatter
 
 # Third party libraries
 import numpy as np
-from PIL import Image
 import matplotlib.pyplot as plt
 
 # Initialize the random seed
@@ -60,31 +55,85 @@ def sigma(h):
 
 # Plot the results of the combined runs using matplotlib
 # I want to try to reuse this from the last lab
-'''def plot_data(experiment_number):
+def plot_data(experiment_number, nvec, avg_stable_prob, avg_unstable_prob, run_no=0):
 
+    run_str = ''
+    p = list(xrange(nvec))
     abs_path = os.path.abspath(".")
     root_path = 'results/data/Experiment-' + str(experiment_number)
     file_path = 'file://' + abs_path
-    path = 'Graph-for-Experiment-' + experiment_number + '.jpg'
+    if run_no == 0:
+        run_str = '-run_no-' + run_no
+    path = 'Graph-for-Experiment-' + experiment_number + run_str +  '.jpg'
 
     fig = plt.figure()
-    axes = fig.add_subplot(111)
 
-    axes.plot(average_p, label='Correlation (rho_l)')
-    axes.plot(average_joint_h, label='Joint Entropy (H_l)')
-    axes.plot(average_mutual_info, label='Mutual Info (I_l)')
-    axes.legend(loc=0)
-    axes.set_position((.1, .3, .8, .6))
-    axes.set_xlabel('l')
-    axes.set_ylabel('Values')
-    axes.set_title('Experiment ' + experiment_number + ' Average Values')
-
-    fig.text(.01, .01, description)
+    # Plot Unstable Imprints
+    plt.subplot(2, 1, 1)
+    plt.plot(p, avg_unstable_prob)
+    plt.legend(loc=0)
+    plt.xlabel('p')
+    plt.ylabel('Fraction of Unstable Imprints')
+    if run_no == 0:
+        plt.title('Overall Fraction of Unstable Imprints for %s Patterns' % nvec)
+    else:
+        plt.title('Fraction of Unstable Imprints for %s Patterns' % nvec)
     plt.grid()
+
+    # Plot Stable Imprints
+    plt.subplot(2, 1, 2)
+    plt.plot(p, avg_stable_prob)
+    plt.legend(loc=0)
+    plt.xlabel('p')
+    plt.ylabel('Fraction of Stable Imprints')
+    if run_no == 0:
+        plt.title('Overall Fraction of Stable Imprints for %s Patters' % nvec)
+    else:
+        plt.title('Fraction of Stable Imprints for %s Patters' % nvec)
+    plt.grid()
+
+    # Save the figure
     fig.savefig(root_path + '/' + path)
 
     return file_path + '/' + root_path + '/' + path
-'''
+
+# Create an html page out of all the JPGs and the Graph
+def create_html_page(experiment_number, graph_list, experiment_graph_path):
+
+    html_filename = 'results/data/Experiment-' + str(experiment_number) + '/Experiment-' + experiment_number + '.html'
+
+    html_string = ''
+
+    string1 = '''
+    <html>
+    <head>
+    <title>Experiment %s</title>
+    </head>
+    <body>
+    <h1>Experiment %s</h1>
+    <ul>
+    ''' % (experiment_number, experiment_number)
+
+    html_string += string1
+    for graph_path in graph_list:
+        graph_path_list = graph_path.split("/")
+        graph_header = graph_path_list[-1]
+        tmp_string = "<h3>%s<h3>" % (graph_header)
+        tmp_string += "<li><img src='%s' height=300 width=300 border=1></li>" % (graph_path)
+        html_string += tmp_string
+
+    string2 ='''
+    </ul>
+    <h2>Average of Runs -- Data Graph</h2>
+    <img src='%s'height=300 width=300 border=1>
+    </body>
+    </html>
+    ''' % (experiment_graph_path)
+
+    html_string += string2
+
+    with open(html_filename, 'w') as hf:
+        hf.write(html_string)
 
 class HNN:
 
@@ -94,17 +143,17 @@ class HNN:
         self.vectors = self.generate_vectors()
         self.vector_size = args.vsize
         self.num_vectors = args.nvec
-        self.stable = np.zeroes((num_vectors)) #array of number of times a
+        self.stable = np.zeroes((self.num_vectors)) #array of number of times a
         self.num_weights = self.vsize * self.vsize
         self.weights = np.zeroes((self.num_weights)) # would be better if we made a 100x100 matrix to copy to NN
         self.nnsize = args.nnsize
         self.NN = np.zeroes((self.nnsize))
-        self.prob_stability = np.zeroes(num_vectors)
-        self.prob_instability = np.zeroes(num_vectors)
-        self.basin_sizes = np.zeroes(num_vectors)
+        self.prob_stability = np.zeroes(self.num_vectors)
+        self.prob_instability = np.zeroes(self.num_vectors)
+        self.basin_sizes = np.zeroes(self.num_vectors)
 
     # Generate the patterms (vectors)
-    def generate_vectors():
+    def generate_vectors(self):
         self.vectors = []
 
         for m in range(self.num_vectors):
@@ -115,30 +164,30 @@ class HNN:
 
         return self.vectors
 
-    def calcStabilityProb(p):
+    def calcStabilityProb(self, p):
         self.prob_stability[p] = self.stable[p]/p
-        self.prob_instability[p] = 1 - prob_stability[p]
+        self.prob_instability[p] = 1 - self.prob_stability[p]
 
-    def getStableProb():
+    def getStableProb(self):
         return self.prob_stability
 
-    def getInstabilityProb():
+    def getInstabilityProb(self):
         return self.prob_instability
 
-    def drive(): #driver for calculating stability (COSC 420)
+    def drive(self): #driver for calculating stability (COSC 420)
         #a. generate vectors
-        generate_vectors()
+        self.generate_vectors()
         for p in range(self.num_vectors):
             #b. imprint the first p vectors on a hopfield newtwork
-            imprint_vectors(p)
+            self.imprint_vectors(p)
             #c. test first p imprinted patterns for stability
-            test_vectors(p)
+            self.test_vectors(p)
             #d. Calculate stability and instability prob for each p
-            calcStabilityProb(p)
+            self.calcStabilityProb(p)
 
     # Step 1 of VanHornwender's Help
     # Check me on this, it may be completely wrong.
-    def imprint_vectors(p):
+    def imprint_vectors(self, p):
         for x in range(p):
             for i in p:
                 for j in p:
@@ -156,44 +205,45 @@ class HNN:
 
     #Started Step 2 of VanHornwender's Help, started to get confused here really late
     # and decided to go to bed.
-    def test_vectors(p):
+    def test_vectors(self, p):
         stable_index = 0
+        h_i = None
         for x in range(p):
             #1. Copy NN into pattern
-            self.NN = vectors[x];
+            self.NN = self.vectors[x];
             new_neuron_state = 0
             stable_bool = True # keep track of stability
 
             #2. Compute new stat value
-            for i in range(nnsize):
+            for i in range(self.nnsize):
                 # h_i = sum[j-1, N]{ w[i][j] * s[j] }
-                for j in range(nnsize):
-                    h_i +=  (weight[i * self.vector_size * i + j] * self.NN[j])
+                for j in range(self.nnsize):
+                    h_i +=  (self.weight[i * self.vector_size * i + j] * self.NN[j])
                 #s'i = sigma(h)
                 new_neuron_state = sigma(h_i)
                 #if they don't match it wasn't stable
                 if self.NN[i] != new_neuron_state:
                     stable_bool = False
                     #427/524 ONLY
-                    basin_sizes[p] = 0
+                    self.basin_sizes[p] = 0
 
                 self.NN[i] = new_neuron_state
 
             #Determine if p is stable: if so increment
             if stable_bool:
-                stable[x] += 1
+                self.stable[x] += 1
                 #427/524 ONLY
-                calc_basin_size(p)
+                self.calc_basin_size(p)
 
-    def calc_basin_size(p):
-        array = np.random.permutation(vector_size)
-        for i in range(num_vectors):
-            p = vectors
+    def calc_basin_size(self, p):
+        array = np.random.permutation(self.vector_size)
+        for i in range(self.num_vectors):
+            p = self.vectors[i]
 
 # Main
 if __name__ == '__main__':
 
-    image_list = []
+    graph_list = []
 
     parser = setup_argparser()
     args = parser.parse_args()
@@ -222,11 +272,18 @@ if __name__ == '__main__':
     for i in range(args.nruns):
         hnn = HNN(args)
         hnn.stability_drive()
+        stable_prob = hnn.getStableProb()
+        unstable_prob = hnn.getInstabilityProb()
+
+        graph_list += plot_data(experiment_number, args.nvec, stable_prob, unstable_prob)
         #sum stable and unstable probs
-        avg_stable_prob += hnn.getStableProb()
-        avg_unstable_prob += hnn.getInstabilityProb()
+        avg_stable_prob += stable_prob
+        avg_unstable_prob += unstable_prob
     #avg stable and unstable probs
     avg_stable_prob /= args.nruns
     avg_unstable_prob /= args.nruns
 
     #graph stable and unstable probs
+    avg_graph_file = plot_data(experiment_number, args.nvec, avg_stable_prob, avg_unstable_prob)
+    create_html_page(experiment_number, graph_list, avg_graph_file)
+
