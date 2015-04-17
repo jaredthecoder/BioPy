b169a92fbd946b31c0f313ca82db7ad83d70e3df
Jared Smith <smith@utk.edu>
2015-04-17 08:59:41 -0400

    updated with genetic algorithm code

---
+++ genetic/genetic.py
@@ -0,0 +1,28 @@
+##############################################################
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# genetic.py, general purpose genetic algorithm in Python
+#
+# Written by Jared Smith and David Cunningham for COSC 427/527
+# at the University of Tennessee, Knoxville
+#
+###############################################################
+
+import os
+import sys
+import argparse
+import json
+import math
+import random
+
+import scipy as sp
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    pass
+
+if __name__=="__main__":
+    main()
